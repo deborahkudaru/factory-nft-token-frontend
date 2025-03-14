d92a76743b8853c89dc2841879643793ff5b236e
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-03-14 11:03:32 +0100

    design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center pb-4  text-black shadow-2xl">
+    <nav className="flex justify-between items-center p-4 rounded-full text-black shadow-2xl">
       <h1 className="text-xl font-bold">Token-NFT Creator</h1>
       <ConnectButton />
     </nav>
